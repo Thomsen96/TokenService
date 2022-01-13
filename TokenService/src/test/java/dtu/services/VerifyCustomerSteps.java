941e622a8db14619bde6265241d43e3646c8cdf2
Squidman95 <squidmanse@gmail.com>
2022-01-13 20:48:05 +0100

    Remove comment test

--- TokenService/src/test/java/dtu/services/VerifyCustomerSteps.java
+++ TokenService/src/test/java/dtu/services/VerifyCustomerSteps.java
@@ -1,6 +1,5 @@
 package dtu.services;
 import static org.junit.Assert.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 

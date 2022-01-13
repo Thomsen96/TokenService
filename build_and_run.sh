3bad578c562371ef574bf2152efbe452425abd67
Thomsen96 <jonastoftethomsen@gmail.com>
2022-01-13 10:38:52 +0100

    Added documentaion for build process

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
+# Build all docker images and mvn 
 ./build.sh
 
-# Update the set of services and
-# build and execute the system tests
-pushd Client
+# Startup all services for this service
 ./deploy.sh 
 sleep 5
+
+# Test the services
 ./test.sh
-popd
 
 # Cleanup the build images
 docker image prune -f

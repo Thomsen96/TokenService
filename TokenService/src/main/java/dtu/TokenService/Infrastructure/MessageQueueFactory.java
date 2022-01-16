0f0f786d51ec1189c29dfe503f7a0e51db64f919
Thomsen96 <jonastoftethomsen@gmail.com>
2022-01-16 17:32:42 +0100

    forgot to add this

---
+++ TokenService/src/main/java/dtu/TokenService/Infrastructure/MessageQueueFactory.java
@@ -0,0 +1,29 @@
+package dtu.TokenService.Infrastructure;
+
+import messaging.MessageQueue;
+import messaging.implementations.RabbitMqQueue;
+
+public class MessageQueueFactory {
+  
+  static RabbitMqQueue messageQueue = null;
+
+	public MessageQueue getMessageQueue() {
+		// The singleton pattern.
+		// Ensure that there is at most
+		// one instance of a PaymentService
+		if (messageQueue != null) {
+			return messageQueue;
+		}
+		
+		// Hookup the classes to send and receive
+		// messages via RabbitMq, i.e. RabbitMqSender and
+		// RabbitMqListener. 
+		// This should be done in the factory to avoid 
+		// the PaymentService knowing about them. This
+		// is called dependency injection.
+		// At the end, we can use the PaymentService in tests
+		// without sending actual messages to RabbitMq.
+		RabbitMqQueue messageQueue = new RabbitMqQueue("localhost");
+		return messageQueue;
+  }
+}
